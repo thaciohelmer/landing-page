d11f5dc9b4900ffdf1080ce4c2c5a0af6b513dd3
Thacio Helmer <thacio.helmer@el.com.br>
2022-11-18 22:11:01 -0300

    Initial commit

---
+++ js/product-viewer.js
@@ -0,0 +1,25 @@
+const imgRotate = document.getElementById('img-rotate')
+const btnPrevious = document.getElementById('btn-previous')
+const btnNext = document.getElementById('btn-next')
+const imgCount = 24
+
+let imgActive = 3
+let mouseOn = false
+
+function imgAlternate(direction) {
+  if (direction) {
+    imgActive == 1 ? imgActive = imgCount : imgActive--
+  } else {
+    imgActive < imgCount ? imgActive++ : imgActive = 1
+  }
+  imgRotate.setAttribute('src', `/img/shoe/Ni_${imgActive}.png`)
+}
+
+function addEventListner(element, action, direction) {
+  element.addEventListener(action, function () {
+    imgAlternate(direction)
+  })
+}
+
+addEventListner(btnNext, 'click', true)
+addEventListner(btnPrevious, 'click', false)
---
+++ js/theme.js
@@ -0,0 +1,61 @@
+const root = document.querySelector(":root");
+const btnTheme = document.querySelector("#btn-theme");
+const brandLogo = document.querySelector("#logo");
+
+const getStyle = (element, style) =>
+  window
+    .getComputedStyle(element)
+    .getPropertyValue(style);
+
+const initialColors = {
+  primaryBgColor: getStyle(root, "--primary-bg-color"),
+  secondaryBgColor: getStyle(root, "--secondary-bg-color"),
+  primaryFtColor: getStyle(root, "--primary-ft-color"),
+  tertiaryBgColor: getStyle(root, "--tertiary-bg-color"),
+  quaternaryFtColor: getStyle(root, "--quaternary-ft-color"),
+  quintenaryFtColor: getStyle(root, "--quintenary-ft-color")
+}
+
+const lightMode = {
+  primaryBgColor: "#f2f2f2",
+  secondaryBgColor: "#e6e6e6",
+  tertiaryBgColor: "#e5e5e5",
+  primaryFtColor: "#242424",
+  quaternaryFtColor: "#0004FF",
+  quintenaryFtColor: "#003CFF"
+}
+
+const transformKey = key => "--" + key.replace(/([A-Z])/g, "-$1").toLowerCase();
+
+const changeTheme = (colors, logo = false) => {
+  Object.keys(colors).map(key =>
+    root.style.setProperty(transformKey(key), colors[key]));
+
+  (logo) ? brandLogo.style.filter = "contrast(60%)" : brandLogo.style.filter = "invert(100%)"
+}
+
+const savePreferences = (active) => localStorage.setItem('theme', active)
+
+const loadSavedPreferences = () =>
+  (localStorage.getItem('theme') === null) ? savePreferences(false) : localStorage.getItem('theme') == 'true';
+
+const loadUserPreferences = () =>
+  (loadSavedPreferences()) ? changeTheme(lightMode, true) : savePreferences(false)
+
+btnTheme.addEventListener("click", () => {
+
+  if (loadSavedPreferences()) {
+
+    savePreferences(false)
+    changeTheme(initialColors)
+
+  } else {
+
+    savePreferences(true)
+
+  }
+
+  loadUserPreferences();
+});
+
+loadUserPreferences();
